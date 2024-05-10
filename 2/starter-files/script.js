79a6d15684242ed66446ea07680cbc41b05050bc
Patopm <egs.pato.pm@gmail.com>
2024-05-10 15:17:19 -0600

    Ejercicio 2 acabado

--- 2/starter-files/script.js
+++ 2/starter-files/script.js
@@ -11,3 +11,30 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+const answer = document.getElementById('answer');
+const ball = document.getElementById('ball');
+const input = document.getElementById('input');
+const error = document.getElementById('error');
+async function apiResponse() {
+    try {
+        if (input.value === '') {
+            setTimeout((error.textContent = 'Please enter a question'), 5000);
+        } else {
+            const response = await fetch(API_ENDPOINT);
+            const data = await response.json();
+            ball.classList.add('shake__ball');
+            answer.textContent = data.answer;
+            setTimeout(() => {
+                ball.classList.remove('shake__ball');
+                answer.textContent = '';
+                input.value = '';
+            }, 5000);
+        }
+    } catch (err) {
+        error.textContent = err.message;
+    }
+}
+
+const button = document.getElementById('button');
+button.addEventListener('click', apiResponse);
