6a0b0b78a2f6373a99b6c6d4d8958041842858db
Patopm <egs.pato.pm@gmail.com>
2024-05-10 15:28:03 -0600

    ejercicio 2 completo

--- 1/app.js
+++ 1/app.js
@@ -1,1 +1,36 @@
 // Escribe tu código aquí.
+const input = document.getElementById("input");
+function palabrasMasLargas(palabra, arrayPalabras) {
+  const palabrasLargas = arrayPalabras.filter((palabraActual) => {
+    return palabraActual.length > palabra.length;
+  });
+  return palabrasLargas;
+}
+
+const lista = document.querySelector("ul");
+function imprimirLista(arrayPalabras) {
+  const lista = document.querySelector("ul");
+  lista.innerHTML = ""; // Limpiar lista antes de agregar nuevos elementos
+
+  arrayPalabras.forEach((palabra) => {
+    const elementoLista = document.createElement("li");
+    elementoLista.textContent = palabra;
+    lista.appendChild(elementoLista);
+  });
+}
+
+const boton = document.querySelector("button");
+
+boton.addEventListener("click", () => {
+  const palabraEntrada = input.value;
+  const myArray = [
+    "insecto",
+    "bootcamp",
+    "mangos",
+    "reptil",
+    "mosca",
+    "escritorio",
+  ];
+  const palabrasLargas = palabrasMasLargas(palabraEntrada, myArray);
+  imprimirLista(palabrasLargas);
+});
